3df788e734f0be90da162667c71280312ed9ca95
Micaela Cavallo <micaela.cavallo@glb-d3512.globant.com>
2015-09-09 11:02:18 -0300

    Estilos + Tabs + Pager + FAB button

--- app/src/main/java/com/example/micaela/huellas/BaseFragment.java
+++ app/src/main/java/com/example/micaela/huellas/BaseFragment.java
@@ -1,7 +1,45 @@
 package com.example.micaela.huellas;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+    private FrameLayout mContentLayout;
+    View rootView;
+    BaseActivity mActivity;
+
+    public final View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                   Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_base, container, false);
+        wireUpLayouts(rootView);
+        View content = this.onCreateEventView(inflater, null, savedInstanceState);
+        mContentLayout.addView(content);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = (BaseActivity) activity;
+    }
+
+
+    public BaseActivity getBaseActivity() {
+        return mActivity;
+    }
+
+    abstract protected View onCreateEventView(LayoutInflater inflater, ViewGroup container,
+                                              Bundle savedInstanceState);
+
+    private void wireUpLayouts(View rootView) {
+        mContentLayout = (FrameLayout) rootView.findViewById(R.id.contentPanel);
+    }
+
 }
--- app/src/main/java/com/example/micaela/huellas/PerdidosFragment.java
+++ app/src/main/java/com/example/micaela/huellas/PerdidosFragment.java
@@ -1,7 +1,15 @@
 package com.example.micaela.huellas;
 
-/**
- * Created by Micaela on 08/09/2015.
- */
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
 public class PerdidosFragment extends BaseFragment  {
+    @Override
+    protected View onCreateEventView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_perdidos, container, false);
+        return view;
+    }
 }
